b1c54dbed0f46828369c045130e0d36b311d01ca
Pedro Costa <pdacosta@fanatics.com>
2017-08-11 10:07:08 -0300

    Adding askForInput method

--- hello.go
+++ hello.go
@@ -7,8 +7,19 @@ import (
 )
 
 func main() {
-	var input string
-	fmt.Print("Enter Your Name: ")
-	fmt.Scanf("%s", &input)
+	input := askForInput("Enter Your Name: ")
 	fmt.Printf("\n%s\n\n", msg.GetGreetingMessage(input))
+	numbersStr := askForInput("Enter some numbers to sum (comma separated): ")
+	sum, err := msg.GetSum(numbersStr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("\n%s\n\n", sum)
+}
+
+func askForInput(message string) string {
+	var input string
+	fmt.Print(message)
+	fmt.Scanln(&input)
+	return input
 }
